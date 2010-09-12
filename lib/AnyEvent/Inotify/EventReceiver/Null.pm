652dc8998c5fc0bf0430116d82c9c0c2dfe80a4e
Jonathan Rockway <jrockway@cpan.org>
2010-09-12 09:14:21 +0000

    Import of JROCKWAY/AnyEvent-Inotify-Simple-0.01 from CPAN.
    gitpan-cpan-distribution: AnyEvent-Inotify-Simple
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JROCKWAY/AnyEvent-Inotify-Simple-0.01.tar.gz
    gitpan-cpan-author:       JROCKWAY
    gitpan-cpan-maturity:     released

---
+++ lib/AnyEvent/Inotify/EventReceiver/Null.pm
@@ -0,0 +1,23 @@
+package AnyEvent::Inotify::EventReceiver::Null;
+use Moose;
+use namespace::autoclean;
+
+with 'AnyEvent::Inotify::EventReceiver';
+
+sub handle_access {}
+sub handle_modify {}
+sub handle_attribute_change {}
+sub handle_close {}
+sub handle_open {}
+sub handle_move {}
+sub handle_delete {}
+sub handle_create {}
+
+1;
+
+__END__
+
+=head1 NAME
+
+AnyEvent::Inotify::EventReceiver::Null - does nothing
+
